b3defcf7ad82e6aaec45ffef821bd300d8e013ac
davydocsurg <davydocsurg@gmail.com>
2020-08-18 14:51:35 +0200

    first

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MonsterGame from '../views/MonsterGame.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'MonsterGame',
+    component: MonsterGame
   },
   {
     path: '/about',
